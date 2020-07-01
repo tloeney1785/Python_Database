be0223779128a555884c99ad1a4e6c63610d7f4a
Tony Lee <tloeney1785@gmail.com>
2020-07-01 14:11:10 -0700

    added all imports

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import ttk
+from PIL import ImageTk, Image
+import pymysql
+import os 
+import shutil 
+import db_config
+
